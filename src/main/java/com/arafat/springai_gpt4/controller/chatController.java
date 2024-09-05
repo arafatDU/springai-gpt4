68008b90ee6df05522582a694f89560461a269eb
ARAFAT <108474267+arafatDU@users.noreply.github.com>
2024-09-06 00:15:20 +0600

    Updated with imageDTO

--- src/main/java/com/arafat/springai_gpt4/controller/chatController.java
+++ src/main/java/com/arafat/springai_gpt4/controller/chatController.java
@@ -2,12 +2,10 @@ package com.arafat.springai_gpt4.controller;
 
 import com.arafat.springai_gpt4.dto.ImageDTO;
 import com.arafat.springai_gpt4.service.ChatService;
-import com.arafat.springai_gpt4.service.GlobalImage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/ai")
@@ -15,9 +13,7 @@ public class chatController {
 
     @Autowired
     ChatService chatService;
-
-    @Autowired
-    GlobalImage globalImage;
+    
 
     @PostMapping("/chat")
     public String generateResponse(@RequestParam String category, String year){
@@ -30,16 +26,5 @@ public class chatController {
         return chatService.getImageChatReader(query, imageDTO.imageBase64());
     }
 
-//    @PostMapping("/image/chat")
-//    public String generateRespImage(@RequestBody Map<String, String> requestBody) throws IOException {
-//        String query = requestBody.get("query");
-//        String imageBase64 = requestBody.get("imageBase64");
-//
-//        // Update the global image base64
-//        if(imageBase64 != null)
-//            globalImage.setImageBase64(imageBase64);
-//
-//        return chatService.getImageChatReader(query);
-//    }
 
 }
--- src/main/java/com/arafat/springai_gpt4/service/ChatService.java
+++ src/main/java/com/arafat/springai_gpt4/service/ChatService.java
@@ -16,8 +16,6 @@ import java.io.IOException;
 import java.util.Base64;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class ChatService {
@@ -25,10 +23,7 @@ public class ChatService {
     @Autowired
     ChatModel chatClient;
 
-    @Autowired
-    GlobalImage globalImage;
 
-    private static final Logger logger = LoggerFactory.getLogger(ChatService.class);
 
     public ChatResponse gentChatResponse(String category, String year){
 
@@ -53,8 +48,6 @@ public class ChatService {
 
     public String getImageChatReader(String query, String imageBase64) {
         // Decode the base64 image to byte array
-        logger.info("Query: {}", query);
-        logger.info("Image Base64: {}", imageBase64);
 
         byte[] imageData = Base64.getDecoder().decode(imageBase64);
 
@@ -65,43 +58,5 @@ public class ChatService {
         return response.getResult().getOutput().getContent();
     }
 
-//    public String getImageChatReader(String query) {
-//        // Get the latest image base64 from GlobalImageStorage
-//        String imageBase64 = globalImage.getImageBase64();
-//
-//        if (imageBase64 == null || imageBase64.isEmpty()) {
-//            throw new IllegalArgumentException("Image data not provided.");
-//        }
-//
-//        byte[] imageData = decodeBase64(imageBase64);
-//
-//        // Create a UserMessage with the query and the image as Media
-//        UserMessage userMessage = new UserMessage(query,
-//                List.of(new Media(MimeTypeUtils.IMAGE_PNG, imageData)));
-//
-//        var response = chatClient.call(new Prompt(userMessage));
-//        return response.getResult().getOutput().getContent();
-//    }
-
-//    private String inferMimeType(String base64) {
-//        if (base64.startsWith("/9j/")) {
-//            return "image/jpeg";
-//        } else if (base64.startsWith("iVBORw0KGgo")) {
-//            return "image/png";
-//        } else if (base64.startsWith("R0lGODlh")) {
-//            return "image/gif";
-//        } else {
-//            // Default to PNG if the type can't be determined
-//            return "image/png";
-//        }
-//    }
-
-    private byte[] decodeBase64(String base64) {
-        // Remove the data URL prefix if it exists
-        if (base64.contains(",")) {
-            base64 = base64.split(",")[1];
-        }
-        return java.util.Base64.getDecoder().decode(base64);
-    }
 }
 
